e1c1e5224790666d7b80d92bf699c33a6bd5d810
okojie micheal <okojiemicheal070@gmail.com>
2022-07-01 09:16:41 -0700

    commit 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,9 +22,9 @@ int main(void)
 	int n;
 
 
-		srand(time(0));
+	srand(time(0));
 
-	   	n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 
         if (n == 0)
 
@@ -40,6 +40,6 @@ int main(void)
 
 
 
-								return (0);
+	return (0);
 
 }
