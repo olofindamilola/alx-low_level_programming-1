33d79152e42a871bc7db52798e554f16455295b5
okojie micheal <okojiemicheal070@gmail.com>
2022-07-05 05:39:02 -0700

    commit 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,30 @@
+#include <main.h>
+
+/**
+ * main:  program that prints _putchar
+ *
+ * description: to write a program that prints _putchar
+ *
+ * Return: (0) Always success
+ *
+ */
+
+
+int main(void)
+{
+	char index[10] = "_putchar"
+	int i = 0;
+
+
+	for (i = 0; i < 9; i++)
+
+	{
+		_putchar(index[i]);
+
+	}
+	
+	_putchar('n');
+
+
+	return (0);
+}
