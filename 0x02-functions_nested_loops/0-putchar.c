5d22aed219c1acd00c4913f39821d093386c4adc
okojie micheal <okojiemicheal070@gmail.com>
2022-07-05 08:50:57 -0700

    commit 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,16 +11,27 @@
 
 
 int main(void)
+
 {
-	char text[10] = "_putchar";
+
+	char text[10] = "Holberton";
+
 	int i = 0;
 
+
+
 	for (i = 0; i < 9; i++)
+
 	{
+
 		_putchar(text[i]);
+
 	}
+
 	_putchar('\n');
 
 
+
 	return (0);
+
 }
