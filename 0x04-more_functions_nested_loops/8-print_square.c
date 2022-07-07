834feb928e863808e670708551c6b5ccd47ce53d
okojie micheal <okojiemicheal070@gmail.com>
2022-07-07 14:15:00 -0700

    commit 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,13 @@
  * Return: void
  */
 
-
 void print_square(int size)
 {
 	int row;
 	int column;
 
 	if (size > 0)
-	
-
+	{
 		for (row = 0; row < size; row++)
 		{
 			for (column = 0; column < size; column++)
